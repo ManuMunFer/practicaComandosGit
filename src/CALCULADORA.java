5cdd2630929d2b953437be72d72bf48eaec24b9c
manu <manumoriles3012@gmail.com>
2024-10-18 08:53:59 +0200

    suma

--- src/CALCULADORA.java
+++ src/CALCULADORA.java
@@ -3,7 +3,7 @@ public class CALCULADORA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Calculadora");
+		System.out.println("Suma");
 	}
 
 }
